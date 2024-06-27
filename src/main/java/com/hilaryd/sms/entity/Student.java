44167d7d15180287a9f132523af4e3604c4aef88
ndifoinhilary <ndifoinhilary@gmail.com>
2024-06-27 16:52:48 +0100

    set up the project and included modelMapper and also the student entity class

---
+++ src/main/java/com/hilaryd/sms/entity/Student.java
@@ -0,0 +1,25 @@
+package com.hilaryd.sms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String email;
+}
