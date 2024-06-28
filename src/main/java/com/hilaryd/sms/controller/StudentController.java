d6c27e9e0e201ad4e4ced7ca3a2a322421954b9a
ndifoinhilary <ndifoinhilary@gmail.com>
2024-06-28 10:17:42 +0100

    Don't with the template to display the students

--- src/main/java/com/hilaryd/sms/controller/StudentController.java
+++ src/main/java/com/hilaryd/sms/controller/StudentController.java
@@ -5,21 +5,29 @@ import com.hilaryd.sms.services.StudentServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/users")
+@RequestMapping("/students")
 @RequiredArgsConstructor
 public class StudentController {
 
     private final StudentServices studentServices;
 
 
+    @GetMapping("")
     public String listUsers(Model model){
         List<StudentDto> students = studentServices.getStudents();
         model.addAttribute("students", students);
         return "students";
+    }
+
+    @GetMapping("/create-form")
+    public  String addStudentForm(Model model){
+        model.addAttribute("student", new StudentDto());
+        return "create_form";
     }
 }
