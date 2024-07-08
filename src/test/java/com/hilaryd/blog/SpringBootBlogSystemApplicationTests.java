5b348ed97736935d68b1b338d9e0cf3ec8b998a0
ndifoinhilary <ndifoinhilary@gmail.com>
2024-07-08 07:03:06 +0100

    Configured the project and added the modelmapper

---
+++ src/test/java/com/hilaryd/blog/SpringBootBlogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilaryd.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
