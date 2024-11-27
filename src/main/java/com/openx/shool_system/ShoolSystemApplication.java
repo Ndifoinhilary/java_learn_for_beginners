f8c2ae66fc07f39ab4d918ee8e326f36a9c98934
ndifoinhilary <ndifoinhilary@gmail.com>
2024-11-28 00:59:08 +0100

    project setup

---
+++ src/main/java/com/openx/shool_system/ShoolSystemApplication.java
@@ -0,0 +1,13 @@
+package com.openx.shool_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoolSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoolSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openx/shool_system/ShoolSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openx.shool_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoolSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
